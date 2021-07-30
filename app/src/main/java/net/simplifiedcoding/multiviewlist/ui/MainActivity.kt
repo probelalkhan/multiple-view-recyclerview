7df0b27a3fa07ab5145ed11117112fbd60d09224
Lakshay Saini <34453079+Lakshay-22699@users.noreply.github.com>
2021-07-30 16:33:41 +0530

    Update MainActivity.kt

--- app/src/main/java/net/simplifiedcoding/multiviewlist/ui/MainActivity.kt
+++ app/src/main/java/net/simplifiedcoding/multiviewlist/ui/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModels<HomeViewModel>()
+    private val homeRecyclerViewAdapter = HomeRecyclerViewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,30 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = homeRecyclerViewAdapter
+        }
+
+        homeRecyclerViewAdapter.itemClickListener = { view, item, position ->
+            val message = when(item){
+                is HomeRecyclerViewItem.Director -> "Director ${item.name} Clicked"
+                is HomeRecyclerViewItem.Movie -> "Movie ${item.title} Clicked"
+                is HomeRecyclerViewItem.Title -> "View All Clicked"
+            }
+            snackbar(message)
+        }
+
+        viewModel.homeListItemsLiveData.observe(this){ result ->
+            when(result){
+                is Resource.Failure -> {
+                    binding.progressBar.hide()
+                    //handle failure case here
+                }
+                Resource.Loading -> binding.progressBar.show()
+                is Resource.Success -> {
+                    binding.progressBar.hide()
+                    homeRecyclerViewAdapter.items = result.value
+                }
+            }
         }
     }
 }
